bfc22d0f779e08fba9f52c03a92552c210b99c29
dpinart <dpinart@uxland.cat>
2020-12-17 01:13:56 +0100

    seed

---
+++ gcp-vars.go
@@ -0,0 +1,99 @@
+package go_gcp_vars
+
+import (
+	"cloud.google.com/go/compute/metadata"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+	"path"
+)
+type GCPVars struct {
+	ProjectId string
+	ServiceName string
+	Port string
+	Debug bool
+}
+
+func init() {
+	loadEnvFile()
+}
+func fileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
+}
+func loadEnvFile() {
+	debug := len(os.Getenv("DEBUG")) > 0
+	if !debug {
+		return
+	}
+	var filename string
+	if fileExists(".env") {
+		filename = ".env"
+	} else {
+		var dir, _ = os.Getwd()
+		for {
+			dir = path.Join(dir, "..")
+			filename = path.Join(dir, ".env")
+			if fileExists(filename) {
+				break
+			}
+			if dir == "" {
+				break
+			}
+		}
+	}
+	_ = godotenv.Load(filename)
+}
+
+func GetGCPVars() *GCPVars {
+	service := os.Getenv("K_SERVICE")
+	if service == "" {
+		service = os.Getenv("GAE_SERVICE")
+		if service == ""{
+			service = "???"
+		}
+
+	}
+
+	revision := os.Getenv("K_REVISION")
+	if revision == "" {
+		revision = "???"
+	}
+
+	project := os.Getenv("GOOGLE_CLOUD_PROJECT")
+
+	// Environment variable GOOGLE_CLOUD_PROJECT is only set locally.
+	// On Cloud Run, strip the timestamp prefix from log entries.
+	if project == "" {
+		log.SetFlags(0)
+	}
+
+	// Only attempt to check the Cloud Run metadata server if it looks like
+	// the service is deployed to Cloud Run or GOOGLE_CLOUD_PROJECT not already set.
+	if project == "" || service != "???" {
+		var err error
+		if project, err = metadata.ProjectID(); err != nil {
+			log.Printf("metadata.ProjectID: Cloud Run metadata server: %v", err)
+		}
+		var ip string
+		if ip, err = metadata.InternalIP(); err == nil{
+			log.Printf("internal IP: %s\n", ip)
+		}
+		if ip, err = metadata.ExternalIP(); err == nil{
+			log.Printf("external IP: %s\n", ip)
+		}
+	}
+	if project == "" {
+		project = "???"
+	}
+	port := os.Getenv("")
+	return &GCPVars{
+		ProjectId:   project,
+		ServiceName: service,
+		Port:        port,
+		Debug:  len(os.Getenv("DEBUG")) > 0,
+	}
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/uxland/go-gcp-vars
+
+go 1.15
+
+require (
+	cloud.google.com/go v0.74.0
+	github.com/joho/godotenv v1.3.0
+)
